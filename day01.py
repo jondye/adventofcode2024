ef1ec1e0663aa43033f7d6e756d519c8c4fdee3d
Jon Dye <jdye@brightsign.biz>
2024-12-06 22:01:13 +0000

    Day 1 part 2

--- day01.py
+++ day01.py
@@ -1,16 +1,26 @@
+from collections import Counter
+import itertools
+
+
 def parse_input(lines):
     return zip(*((int(v) for v in l.split()) for l in lines.splitlines()))
 
 def diff(a, b):
     return (abs(x-y) for (x,y) in zip(sorted(a), sorted(b)))
 
-def day01(lines):
-    lists = parse_input(lines)
+def part1(lists):
     return sum(diff(*lists))
+
+def part2(lists):
+    left, right = lists
+    counts = Counter(right)
+    return sum(val * counts[val] for val in left)
 
 def main():
     with open("input01.txt") as f:
-        print(day01(f.read()))
+        lists1, lists2 = itertools.tee(parse_input(f.read()))
+        print(f"Part 1: {part1(lists1)}")
+        print(f"Part 2: {part2(lists2)}")
 
 if __name__ == '__main__':
     main()
--- test_day01.py
+++ test_day01.py
@@ -1,9 +1,9 @@
 from hamcrest import *
-
-from day01 import *
+import pytest
+import day01
 
 def test_day01():
-    data = """
+    day01_input = """
         3   4
         4   3
         2   5
@@ -11,23 +11,16 @@ def test_day01():
         3   9
         3   3
         """.strip()
-    assert day01(data) == 11
 
-def test_parse_input():
-    data = """
-        3   4
-        4   3
-        2   5
-        1   3
-        3   9
-        3   3
-        """.strip()
-    parsed_input = parse_input(data)
     assert_that(
-        parsed_input,
+        day01.parse_input(day01_input),
         contains_exactly(
             contains_exactly(3, 4, 2, 1, 3, 3),
             contains_exactly(4, 3, 5, 3, 9, 3)))
 
-def test_diff():
-    assert_that(diff((2,1,4,3), (7,7,5,3)), contains_exactly(2, 3, 4, 3))
+    assert_that(day01.diff((2,1,4,3), (7,7,5,3)), contains_exactly(2, 3, 4, 3))
+
+    lists = ((3, 4, 2, 1, 3, 3), (4, 3, 5, 3, 9, 3))
+    assert_that(day01.part1(lists), is_(11))
+
+    assert_that(day01.part2(lists), is_(31))
