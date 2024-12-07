4f7cf7c41fb24008e984c008e8e74b4f65cffb44
Jon Dye <jdye@brightsign.biz>
2024-12-07 07:25:36 +0000

    Day 3 part 2

--- day03.py
+++ day03.py
@@ -3,12 +3,26 @@
 import re
 
 
-def part1(input):
-    return sum(int(x) * int(y) for x, y in re.findall(r"mul\((\d+),(\d+)\)", input))
+def part1(text):
+    return sum(int(x) * int(y) for x, y in re.findall(r"mul\((\d+),(\d+)\)", text))
+
+
+def disable(text):
+    return re.sub(r"don't\(\).*?(do\(\)|$)", "", text, flags=re.DOTALL)
+
+
+def part2(text):
+    return sum(
+        int(x) * int(y) for x, y in re.findall(r"mul\((\d+),(\d+)\)", disable(text))
+    )
+
 
 def main():
     with open("input03.txt") as f:
-        print(f"Part 1: {part1(f.read())}")
+        text = f.read()
+        print(f"Part 1: {part1(text)}")
+        print(f"Part 2: {part2(text)}")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
--- test_day03.py
+++ test_day03.py
@@ -1,8 +1,27 @@
+from textwrap import dedent
 import day03
 
 from hamcrest import assert_that, is_
 
 
 def test_day03():
-    input = r"xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+    input = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
+
     assert_that(day03.part1(input), is_(161))
+
+    assert_that(day03.disable(input), is_("xmul(2,4)&mul[3,7]!^?mul(8,5))"))
+    assert_that(day03.disable("mul(2,4)don't()mul(3,2)"), is_("mul(2,4)"))
+    assert_that(
+        day03.disable("mul(1,2)don't()mul(3,4)do()mul(5,6)do()mul(7,8)"),
+        is_("mul(1,2)mul(5,6)do()mul(7,8)"),
+    )
+    assert_that(
+        day03.disable("mul(1,2)don't()mul(3,4)don't()mul(5,6)do()mul(7,8)"),
+        is_("mul(1,2)mul(7,8)"),
+    )
+    assert_that(
+        day03.disable("mul(1,2)don't()mul(3,4)\nmul(5,6)do()mul(7,8)"),
+        is_("mul(1,2)mul(7,8)"),
+    )
+
+    assert_that(day03.part2(input), is_(48))
